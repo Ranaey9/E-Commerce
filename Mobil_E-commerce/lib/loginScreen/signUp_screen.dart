3e6fe048b41d421265dd942239963de8d1b7524e
rana eyp <eyrana002@gmail.com>
2026-03-21 22:44:27 +0300

    Update UI Sign Up screen

---
+++ Mobil_E-commerce/lib/loginScreen/signUp_screen.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController surnameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isObscured = true;
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: screenHeight * 0.13,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF2B2E81),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: screenHeight * 0.13,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: screenWidth * 0.35,
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: screenWidth * 0.30),
+
+            const Text(
+              "Shop-Store",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF2B2E81),
+              ),
+            ),
+            const SizedBox(height: 40),
+
+            //name
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                textInputAction: TextInputAction.next,
+                controller: nameController,
+                cursorColor: const Color(0xFF2B2E81),
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  floatingLabelStyle: const TextStyle(color: Color(0xFF2B2E81)),
+                  filled: true,
+                  fillColor: const Color(0xFFF5F5F7),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF2B2E81),
+                      width: 1.5,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 15,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            //surname
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                textInputAction: TextInputAction.next,
+                controller: surnameController,
+                cursorColor: const Color(0xFF2B2E81),
+                decoration: InputDecoration(
+                  labelText: "Surname",
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  floatingLabelStyle: const TextStyle(color: Color(0xFF2B2E81)),
+                  filled: true,
+                  fillColor: const Color(0xFFF5F5F7),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF2B2E81),
+                      width: 1.5,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 15,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // E-mail
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                controller: _emailController,
+                cursorColor: const Color(0xFF2B2E81),
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  floatingLabelStyle: const TextStyle(color: Color(0xFF2B2E81)),
+                  filled: true,
+                  fillColor: const Color(0xFFF5F5F7),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF2B2E81),
+                      width: 1.5,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 15,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: _isObscured,
+                textInputAction: TextInputAction.done,
+                cursorColor: const Color(0xFF2B2E81),
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: const TextStyle(color: Colors.grey),
+                  floatingLabelStyle: const TextStyle(color: Color(0xFF2B2E81)),
+                  filled: true,
+                  fillColor: const Color(0xFFF5F5F7),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isObscured = !_isObscured;
+                      });
+                    },
+                    icon: Icon(
+                      _isObscured ? Icons.visibility : Icons.visibility_off,
+                      color: _isObscured
+                          ? Colors.grey
+                          : const Color(0xFF2B2E81),
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: const BorderSide(
+                      color: Color(0xFF2B2E81),
+                      width: 1.5,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 15,
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: InkWell(
+                onTap: () {
+                  print("Sign Up clicked");
+                },
+                borderRadius: BorderRadius.circular(15),
+                child: Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF2B2E81),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Aleardy have a account?",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Color(0xFF2B2E81),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
