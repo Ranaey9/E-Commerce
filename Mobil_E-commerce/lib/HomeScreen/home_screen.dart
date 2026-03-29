461201c9461a72754611cab29c704351846e9a6f
rana eyp <eyrana002@gmail.com>
2026-03-29 17:52:04 +0300

    add home screen with search bar and custom bottom nav

--- Mobil_E-commerce/lib/HomeScreen/home_screen.dart
+++ Mobil_E-commerce/lib/HomeScreen/home_screen.dart
@@ -14,16 +14,88 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu, color: Colors.black, size: 30),
+        ),
         actions: [
           IconButton(
             onPressed: () {},
             icon: const Icon(Icons.notifications, color: Colors.black),
             iconSize: 30,
           ),
-          const SizedBox(width: 10), // En sağdan biraz boşluk bırakır
+          const SizedBox(width: 10),
         ],
       ),
-      body: const Center(child: Text("Home Content")),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search ...',
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  contentPadding: EdgeInsets.symmetric(vertical: 15),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsGeometry.symmetric(
+              horizontal: 15,
+              vertical: 10,
+            ),
+            child: Container(
+              height: 140,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          border: Border(top: BorderSide(color: Colors.grey, width: 0.5)),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 8),
+            BottomNavigationBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: const Color(0xFF2B2E81),
+              unselectedItemColor: Colors.grey,
+              currentIndex: 0,
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.grid_view_rounded),
+                  label: '',
+                ),
+                BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.local_mall_outlined),
+                  label: '',
+                ),
+                BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
